15057b5b05b7a198d2f63a71acffbfba49aa175b
Jae-Hong-Kim <newfly101@naver.com>
2024-12-05 10:38:01 +0900

    first commit : 초기 Netfilx 구성 (react-router-dom) 이용해 view구상 완료

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,29 @@
-function App() {
+import React from "react";
+import {Outlet, Routes, Route} from "react-router-dom";
+
+const LayOut = () => {
   return (
-  <div>App.js</div>
+      <>
+        <Nav />
+
+        <Outlet />
+
+        <Footer />
+      </>
+  )
+}
+
+const App = () => {
+  return (
+  <div className="App">
+    <Routes>
+      <Route path="/" component={<LayOut />}>
+          <Route path="/" component={<MainPage />} />
+          <Route path="/" component={<DetailPage />} />
+          <Route path="search" component={<SearchPage />} />
+      </Route>
+    </Routes>
+  </div>
   );
 }
 
