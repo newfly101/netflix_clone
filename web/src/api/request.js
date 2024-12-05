459c30e1d18a04b219f7371001cc6229f503fae4
Jae-Hong-Kim <newfly101@naver.com>
2024-12-05 14:45:21 +0900

    [3] API 요청할 때 parameter 값 연결하는 구문도 추가

--- web/src/api/request.js
+++ web/src/api/request.js
@@ -1,5 +1,6 @@
 const requests = {
     fetchNowPlaying: `movie/now_playing`,
+    fetchMovieDetail: (id) => `movie/${id}`,
     fetchNetflixOriginals: `/discover/tv?with_networks=213`,
     fetchTrending: `/trending/all/week`,
     fetchTopRated: `/movie/top_rated`,
