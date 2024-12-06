9da9e85a2b9c0babbc01fc32f882e83075db061d
Jae-Hong-Kim <newfly101@naver.com>
2024-12-06 16:26:18 +0900

    [D1-F] useMemo() 를 통해 dependencies 연동할 때 warning 제거

--- web/src/components/common/YoutubePlayer.js
+++ web/src/components/common/YoutubePlayer.js
@@ -1,12 +1,12 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef} from 'react';
 import YouTubeLoad from "../../utils/YouTubeLoad";
 
 const YoutubePlayer = (props) => {
     const {videoId, width, height} = props;
-
     const videoRef = useRef(null);
 
-    const defaultOptions = {
+    const dimensions = useMemo(() => ({width, height}), [width, height]);
+    const getDefaultOptions = useCallback(() => ({
         autoplay:1,             // 자동 재생 여부 (0: 비활성화, 1: 활성화)
         controls:0,             // 플레이어 컨트롤 표시 여부 (0: 숨김, 1: 표시)
         loop:1,                 // 동영상 반복 여부 (1: 반복)
@@ -18,10 +18,13 @@ const YoutubePlayer = (props) => {
             rel: 0,             // 재생 후 관련 동영상 표시 여부 (0: 표시 안 함)
             showinfo: 0,        // 동영상 정보 표시 여부 (0: 숨김)
             modestbranding: 1,  // YouTube 로고 최소화 (1: 최소화)
+            origin: window.location.origin, // 현재 origin 설정
         }
-    }
+    }), []);
+
     useEffect(() => {
         YouTubeLoad();
+        const defaultOptions = getDefaultOptions();
 
         window.onYouTubeIframeAPIReady = () => {
             console.log("YouTubeIframeAPIReady, videoId:",videoId);
@@ -35,8 +38,7 @@ const YoutubePlayer = (props) => {
                     mute: defaultOptions.mute,
                     ...defaultOptions.playerVars,
                 },
-                width: width,
-                height: height,
+                ...dimensions,
             });
         };
         return () => {
@@ -44,7 +46,7 @@ const YoutubePlayer = (props) => {
                 videoRef.current.destroy();
             }
         };
-    }, [videoId, defaultOptions]);
+    }, [videoId, dimensions, getDefaultOptions]);
 
     return <div id="youtube-player"/>;
 };
