99a283ceeb7cef74752279899f43005c0bc6ff09
Jae-Hong-Kim <newfly101@naver.com>
2024-12-06 12:17:54 +0900

    [D] youtube iframe 을 넣는 경우 console에 warning을 제거하기 위했던 시도

--- web/src/view/MainComponent/VideoFrame.js
+++ web/src/view/MainComponent/VideoFrame.js
@@ -7,10 +7,13 @@ const VideoFrame = ({movie}) => {
     return (
         <div className={styles.container}>
             <iframe
-                src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+                src={`https://www.youtube-nocookie.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
                 width="100%"
                 height="800"
                 allow="autoplay; fullscreen"
+                title="Youtube Video"
+                sandbox="allow-same-origin allow-scripts allow-popups allow-forms allow-storage-access-by-user-activation"
+                loading="lazy"
             ></iframe>
         </div>
     );
