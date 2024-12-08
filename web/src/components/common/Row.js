025962e7336c64a0949bbefe5df1ce63f6b59774
Jae-Hong-Kim <newfly101@naver.com>
2024-12-08 18:15:03 +0900

    [4] Row SlideBar 관련 css옵션 조정

--- web/src/components/common/Row.js
+++ web/src/components/common/Row.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import instance from "../../api/axios";
-import styles from "../main/Main.module.css";
+import styles from "./Row.module.css";
 
 const imgUrl = process.env.REACT_APP_GET_MOVIE_POSTER_UTL;
 
@@ -13,33 +13,35 @@ const Row = (props) => {
     }, []);
 
     const fetchData = async () => {
-        const {data: {results : request }} = await instance.get(url);
+        const {data: {results: request}} = await instance.get(url);
         // const {data: request} = await instance.get(url);
         // todo 여기서 state 값 [{},{}] 로 변경 후 detail page만들 것
-        console.log("request",request);
+        console.log("request", request);
         isLarge ? setPosts(request.map(item => item?.poster_path))
-                : setPosts(request.map(item => item.backdrop_path || item.poster_path))
+            : setPosts(request.map(item => item.backdrop_path || item.poster_path))
         // setPosts(request.map(item => item?.poster_path));
     }
 
     return (
         <div className={styles.container}>
-            <div className={styles.itemContainer}>
+            <section className={styles.itemContainer}>
                 <h1 className={styles.itemTitle}>{title}</h1>
-                {isLarge ? (
-                    <div className={styles.largeItems}>
-                        {posts.map((item, i) => (
-                            <img className={styles.largeItem} key={i} src={`${imgUrl}${item}`} alt="d"/>
-                        ))}
+                <div style={{ position: "relative" }}>
+                    <div className={styles.arrowLeft}>
+                        <span>{"<"}</span>
                     </div>
-                ) : (
-                    <div className={styles.smallItems}>
-                        {posts.map((item, i) => (
-                            <img className={styles.smallItem} key={i} src={`${imgUrl}${item}`} alt="d"/>
+                    <article className={isLarge ? styles.largeItems : styles.smallItems}>
+                    {posts.map((item, i) => (
+                            <img className={isLarge ? styles.largeItem : styles.smallItem} key={i}
+                                 src={`${imgUrl}${item}`}
+                                 alt="img"/>
                         ))}
+                    </article>
+                    <div className={`${styles.arrowRight}`}>
+                        <span>{">"}</span>
                     </div>
-                )}
-            </div>
+                </div>
+            </section>
         </div>
     );
 };
