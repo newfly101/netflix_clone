572deeb02c9bb9c72fab88f50aedf441c3f9c648
Jae-Hong-Kim <newfly101@naver.com>
2024-12-09 00:11:38 +0900

    [6] 검색 결과를 useLocation으로 검색 페이지로 전달하고, 검색페이지에서 params를 가지고 가공

--- web/src/components/search/Search.js
+++ web/src/components/search/Search.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import styles from "./Search.module.css";
+import {useLocation} from "react-router-dom";
 
 const Search = () => {
+    console.log(useLocation());
+    const useQuery = () => {
+        return new URLSearchParams(useLocation().search);
+    }
+    let query = useQuery();
+    const searchTerm = query.get("q");
+    console.log(`searchTerm: ${searchTerm}`);
     return (
         <div>
             SearchPage
