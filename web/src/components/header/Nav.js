d0469b33cafb2854c8d6e3f4e6e91c904187ec94
Jae-Hong-Kim <newfly101@naver.com>
2024-12-09 00:07:40 +0900

    [6] Nav에 searchBox 추가

--- web/src/components/header/Nav.js
+++ web/src/components/header/Nav.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import styles from './Nav.module.css';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 const Nav = () => {
+    const [searchValue, setSearchValue] = React.useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = useCallback((value) => {
+        console.log(value);
+        setSearchValue(value);
+        navigate(`/search?q=${value}`);
+    }, [searchValue]);
+
     return (
         <div className={styles.container}>
             <Link to="/" target="_self">
                 <img className={styles.netflixLogo} src="/Netflix_Logo.png" alt="Netflix logo"/>
             </Link>
             <div className={styles.buttonContainer}>
+                <input type="search"
+                       value={searchValue}
+                       onChange={(e) => handleSearch(e.target.value)}
+                       />
                 <div className={styles.translateLang}>
                     <img src="/assets/lang.svg" alt="lang"/>
                     <span>한국어</span>
