48aea74f6ded33a8071456cb603ecf77ff7b9e78
Jae-Hong-Kim <newfly101@naver.com>
2024-12-09 13:53:04 +0900

    [F] warning 제거

--- web/src/App.js
+++ web/src/App.js
@@ -3,7 +3,6 @@ import {Outlet, Routes, Route} from "react-router-dom";
 import Nav from './components/header/Nav';
 import Footer from './components/footer/Footer';
 import MainPage from "./components/main/Main";
-import DetailPage from "./components/detail/Detail";
 import SearchPage from "./components/search/Search";
 import styles from "./App.module.css";
 
@@ -25,7 +24,6 @@ const App = () => {
     <Routes>
       <Route path="/" element={<LayOut />}>
           <Route index element={<MainPage />} />
-          <Route path="detail" element={<DetailPage />} />
           <Route path="search" element={<SearchPage />} />
       </Route>
     </Routes>
