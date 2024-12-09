06a8b67e6fecd80313724d0a20d367bc00a4f587
Jae-Hong-Kim <newfly101@naver.com>
2024-12-09 10:50:53 +0900

    [6] config 에서 공통으로 참조하는 URL 전역으로 import해주기 위해서 값 정의

---
+++ web/src/config/config.js
@@ -0,0 +1,3 @@
+export const API_URL = process.env.REACT_APP_NETFLIX_API_URL;
+export const API_KEY = process.env.REACT_APP_NETFLIX_API_KEY;
+export const IMAGE_UTL = process.env.REACT_APP_GET_MOVIE_POSTER_UTL;
