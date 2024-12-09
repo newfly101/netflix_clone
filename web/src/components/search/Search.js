261ef51ee91147cf9506ac8aa96baa1c77a4fc07
Jae-Hong-Kim <newfly101@naver.com>
2024-12-09 10:39:50 +0900

    [6] 검색text가 바뀔때 마다 새로운 결과 도출, 결과 페이지 디자인

--- web/src/components/search/Search.js
+++ web/src/components/search/Search.js
@@ -4,18 +4,19 @@ import {useLocation} from "react-router-dom";
 import instance from "../../api/axios";
 import requests from "../../api/request";
 
+const imgUrl = process.env.REACT_APP_GET_MOVIE_POSTER_UTL;
 const Search = () => {
-    const [searchText, setSearchText] = React.useState([]);
-    console.log(useLocation());
+    const [searchResult, setSearchResult] = React.useState([]);
+    // console.log(useLocation());
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     }
     let query = useQuery();
     const searchTerm = query.get("q");
-    console.log(`searchTerm: ${searchTerm}`);
+    // console.log(`searchTerm: ${searchTerm}`);
 
     useEffect(() => {
-        if (searchTerm) {
+        if (searchTerm.trim().length > 0) {
             fetchSearchMovie(searchTerm);
         }
     }, [searchTerm]);
@@ -23,6 +24,7 @@ const Search = () => {
     const fetchSearchMovie = async (searchTerm) => {
         try {
             const {data: {results : request}} = await instance.get(requests.fetchSearchMovies(searchTerm));
+            setSearchResult(request);
             console.log(request);
         } catch (e) {
             console.error(e);
@@ -30,9 +32,25 @@ const Search = () => {
     }
 
     return (
-        <div>
-            SearchPage
-        </div>
+        <>
+        {searchTerm.trim().length > 0 && searchResult.length > 0
+            ? <section className={styles.container}>
+                <div className={styles.searchResult}>
+                    {searchResult.map((item, i) => (
+                        item.poster_path && <img key={item.id} src={`${imgUrl}${item?.poster_path}`} alt="img"/>
+                    ))}
+                </div>
+            </section>
+            : (
+            <section className={styles.container}>
+                <div className={styles.searchNoResult}>
+                    <p> 검색 결과 "{searchTerm}" 와 연관된 영화가 없습니다.</p>
+                    <p> 다른 키워드로 검색하세요.</p>
+                </div>
+            </section>
+            )
+        }
+        </>
     );
 };
 
