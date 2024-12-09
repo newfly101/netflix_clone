ef77d8f672f9bb0e9c1eb35dfb2301cca8a44f04
Jae-Hong-Kim <newfly101@naver.com>
2024-12-09 16:11:55 +0900

    🎨[REDUX]🎨 REDUX REDUCER 설정 STATE -> ACTION.TYPE -> ACTION -> REDUCER로 state값 변경 처리하는 부분 코드 추가

--- web/redux/main-reducer.js
+++ web/redux/main-reducer.js
@@ -1,11 +1,28 @@
+import {actionTypes} from "./actionTypes";
 
-// 초기 state ->
+
+// 초기 state
 const initialState = {
+    movieList: [],
     movie: [],
     isClicked: false,
     isModalOpen: false,
-    selectedMovie: [],
 }
 
+const mainReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.GET_MOVIE_LIST:
+            return { ...state, movieList: action.payload };
+        case actionTypes.GET_MOVIE:
+            return { ...state, movie: action.payload };
+        case actionTypes.GET_MOVIE_PLAYER:
+            return { ...state, isClicked: action.payload };
+        case actionTypes.GET_MOVIE_MODAL_OPEN:
+            return { ...state, isModalOpen: action.payload };
+        default:
+            return state;
+    }
+}
 
+export default mainReducer;
 
